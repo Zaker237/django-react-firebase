0a022932b54967207142e6a1a8289f3f3e5bb751
Alex <mboutchouangalex+github@gmail.com>
2023-12-22 23:05:08 +0100

    feat: add decorator to verify firebase auth

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -2,12 +2,28 @@
 import React, {useContext} from "react";
 import 'firebase/compat/auth';
 import { AuthContext } from "../context/AuthContext";
-//import firebase from 'firebase/compat/app'
+import firebase from 'firebase/compat/app'
 // import { auth } from "../firebase/firebase";
 
 
 export const HomePage: React.FC = () => {
     const user = useContext(AuthContext);
+
+	const getData = () => {
+		firebase.auth().onAuthStateChanged(async (user) => {
+			const token = await user?.getIdToken();
+			fetch("http://127.0.0.1:8000/api/todolists?format=json", {
+				headers: {
+					"Authotization": `Bearer ${token}`
+				}
+			}
+			)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			})
+		})
+	}
 	return (
 		<div
 			className="flex flex-col w-full h-full items-center justify-end bg-slate-300"
@@ -15,6 +31,9 @@ export const HomePage: React.FC = () => {
             <p className="p-3 text-white text-xl">
                 Welcome { user ? user.displayName: ""}
             </p>
+			<button className="bg-green-600 text-white p-2" onClick={getData}> 
+				Get Data from backend
+			</button>
 		</div>
 	);
 };
