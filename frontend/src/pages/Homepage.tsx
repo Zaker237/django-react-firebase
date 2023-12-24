61c8ad63655e80a38a838250cc2a82f3a99085fc
Alex <mboutchouangalex+github@gmail.com>
2023-12-24 15:10:59 +0100

    use axios to make request on frontend

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,37 +1,38 @@
 
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import 'firebase/compat/auth';
 import { AuthContext } from "../context/AuthContext";
 import firebase from 'firebase/compat/app'
+import axios from "axios";
 // import { auth } from "../firebase/firebase";
 
 
 export const HomePage: React.FC = () => {
-    const user = useContext(AuthContext);
+	const user = useContext(AuthContext);
 
-	const getData = () => {
+	const getData = async () => {
 		firebase.auth().onAuthStateChanged(async (user) => {
 			const token = await user?.getIdToken();
-			fetch("http://127.0.0.1:8000/api/todolists?format=json", {
-				headers: {
-					"Authotization": `Bearer ${token}`
+			const response = await axios.get(
+				"http://127.0.0.1:8000/api/todolists",
+				{
+					headers: {
+						"Authorization": `Bearer ${token}`,
+						"Content-Type": "application/json"
+					}
 				}
-			}
-			)
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-			})
+			);
+			console.log(response.data)
 		})
 	}
 	return (
 		<div
 			className="flex flex-col w-full h-full items-center justify-end bg-slate-300"
 		>
-            <p className="p-3 text-white text-xl">
-                Welcome { user ? user.displayName: ""}
-            </p>
-			<button className="bg-green-600 text-white p-2" onClick={getData}> 
+			<p className="p-3 text-white text-xl">
+				Welcome {user ? user.displayName : ""}
+			</p>
+			<button className="bg-green-600 text-white p-2" onClick={getData}>
 				Get Data from backend
 			</button>
 		</div>
